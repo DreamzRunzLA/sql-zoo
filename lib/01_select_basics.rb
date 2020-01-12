3a52e9faabb5363bd689183eb1876473867b8330
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2020-01-11 19:26:46 -0700

    Finished section 1

--- lib/01_select_basics.rb
+++ lib/01_select_basics.rb
@@ -8,7 +8,7 @@
 #  population  :integer
 #  gdp         :integer
 #  test change to repo
-# change 2
+#  change 2
 
 require_relative './sqlzoo.rb'
 
@@ -38,6 +38,9 @@ def per_capita_gdp
   # Show the name and per capita gdp (gdp/population) for each country where
   # the area is over 5,000,000 km^2
   execute(<<-SQL)
+    select name, (gdp/population) as per_capita_gdp
+    from countries
+    where area > 5000000
   SQL
 end
 
@@ -45,6 +48,9 @@ def small_and_wealthy
   # Show the name and continent of countries where the area is less than 2,000
   # and the gdp is more than 5,000,000,000.
   execute(<<-SQL)
+    select name, continent
+    from countries
+    where area < 2000 AND gdp > 5000000000
   SQL
 end
 
@@ -52,12 +58,18 @@ def scandinavia
   # Show the name and the population for 'Denmark', 'Finland', 'Norway', and
   # 'Sweden'
   execute(<<-SQL)
+    select name, population
+    from countries
+    where name in ('Denmark', 'Finland', 'Norway', 'Sweden')
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
+    select name
+    from countries
+    where left(name, 1) LIKE 'G'
   SQL
 end
 
@@ -66,5 +78,8 @@ def just_the_right_size
   # with an area between 200,000 and 250,000.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
+    select name, (area/1000) as thou
+    from countries
+    where area between '200000' and '250000'
   SQL
 end
